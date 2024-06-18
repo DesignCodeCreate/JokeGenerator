3df32bd94f272720fbcb1613d2e2ead47fd87488
Owen Hoskin <hoskinowen7@gmail.com>
2024-06-18 08:53:17 +0100

    change layout and fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,25 +25,39 @@
 	<title>Joke Generator Website!</title>
 </svelte:head>
 
+<style>
+	.lexend {
+		font-family: "Lexend", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 500;
+		font-style: normal;
+	}
+
+</style>
+
 <div class="mx-auto w-full text-center w-full top-0 left-0 z-40 fixed mt-0">
 	<div class="dark:bg-slate-50 bg-white shadow-md p-4 mb-4">
 		<div dir="rtl">
 			<div class="inline-block">
 				<div class="flex text-center items-center">
-					<h1 class="text-[#A284B6] text-4xl">
+					<h1 class="text-[#A284B6] text-2xl lexend">
 						<b>!Joke Generator</b>
 					</h1>
 				</div>
 			</div>
 
 		</div>
-		{#if data}
-			<p>{data.type === 'single' ? data.joke : `${data.setup} - ${data.delivery}`}</p>
-		{:else if error}
-			<p>Error: {error}</p>
-		{:else}
-			<p>Loading...</p>
-		{/if}
 	</div>
+
+	{#if data}
+		<p class="lexend">{data.type === 'single' ? data.joke : `${data.setup} - ${data.delivery}`}</p>
+	{:else if error}
+		<p class="lexend">Error: {error}</p>
+	{:else}
+		<p class="lexend">Loading...</p>
+	{/if}
+
 </div>
+
+
 
